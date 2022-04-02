3f1a1e3910b6e97b242febbbfa005f5bd1c7ba4d
marecabo <23156476+marecabo@users.noreply.github.com>
2022-04-02 14:42:50 +0200

    WIP: Move PtNetworkSimplifier from vsp contrib

---
+++ matsim/src/test/java/org/matsim/pt/utils/PtNetworkSimplifierTest.java
@@ -0,0 +1,14 @@
+package org.matsim.pt.utils;
+
+public class PtNetworkSimplifierTest {
+
+	// Todo: Sioux Falls
+
+	// Todo: test merging of links with one transitroute
+
+	// Todo: test merging of links with multiple transitroute
+
+	// Todo: test merging of links with multiple transitroute, where it should not
+	// happen
+
+}
